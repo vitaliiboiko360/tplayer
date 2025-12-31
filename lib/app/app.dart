830506e8f602cdb35879a0b99d885cb22519e09a
vitalii boiko <vukelan@gmail.com>
2025-12-31 03:33:43 +0200

    add router

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,28 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:tplayer/app/home.dart';
+import 'package:go_router/go_router.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Text Player',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 168, 168, 168),
-        ),
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-            fontSize: 20.0,
-            color: Colors.black,
-            fontWeight: FontWeight.normal,
+    return MaterialApp.router(routerConfig: _router);
+  }
+}
+
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return MaterialApp(
+          title: 'Text Player',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 168, 168, 168),
+            ),
+            textTheme: const TextTheme(
+              bodyMedium: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+              ),
+              // You can define other styles like headlineLarge, etc.
+            ),
           ),
-          // You can define other styles like headlineLarge, etc.
+          home: const HomePage(),
+        );
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'details',
+          builder: (BuildContext context, GoRouterState state) {
+            return const DetailsScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+class DetailsScreen extends StatelessWidget {
+  /// Constructs a [DetailsScreen]
+  const DetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Details Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => context.go('/'),
+          child: const Text('Go back to the Home screen'),
         ),
       ),
-      home: const HomePage(),
     );
   }
 }
