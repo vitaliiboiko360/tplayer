82880859241c6856f14f723db3327dec7cd5dbd1
vitalii boiko <vukelan@gmail.com>
2026-01-01 04:14:25 +0200

    add testing catalog

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -13,6 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.sizeOf(context).height;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -26,6 +27,7 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Catalog(),
+      floatingActionButton: Text('$screenHeight'),
     );
   }
 }
@@ -47,10 +49,49 @@ class ContainerCatalog extends StatelessWidget {
     return Center(
       child: Container(
         margin: EdgeInsets.all(0.0), // Space outside the container
-        width: 400,
+        width: 500,
         padding: EdgeInsets.all(15.0), // Space inside the container
         decoration: BoxDecoration(color: Colors.lightBlue[100]),
+        child: InnerCatalog(),
       ),
+    );
+  }
+}
+
+final List<String> entries = List<String>.generate(
+  20,
+  (int index) => ['A', 'B', 'C'][index % 3],
+  growable: true,
+);
+final List<int> colorCodes = List<int>.generate(
+  20,
+  (int index) => 100 * ((index % 9) + 1),
+  growable: true,
+);
+
+class InnerCatalog extends StatelessWidget {
+  const InnerCatalog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: entries.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: EdgeInsetsGeometry.directional(
+            start: 0,
+            top: 10,
+            bottom: 10,
+            end: 0,
+          ),
+          child: Container(
+            height: 100,
+            color: Colors.amber[colorCodes[index]],
+            child: Center(child: Text('Entry ${entries[index]}')),
+          ),
+        );
+      },
     );
   }
 }
