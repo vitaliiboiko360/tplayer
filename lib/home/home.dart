342cbae339216dcaea2b2fabbd3db49fcf1a8bb5
vitaliiboiko360 <vukelan@gmail.com>
2026-02-14 23:52:28 +0200

    add slide changer

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -98,7 +98,51 @@ class InnerCatalog extends StatelessWidget {
 class Slider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(child:);
+    return Container(child: SlideChanger());
   }
 }
 
+class SlideChanger extends StatefulWidget {
+  const SlideChanger({super.key});
+
+  @override
+  State<SlideChanger> createState() => _AnimatedSwitcherExampleState();
+}
+
+class _AnimatedSwitcherExampleState extends State<SlideChanger> {
+  int _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return ScaleTransition(scale: animation, child: child);
+            },
+            child: Text(
+              '$_count',
+              // This key causes the AnimatedSwitcher to interpret this as a "new"
+              // child each time the count changes, so that it will begin its animation
+              // when the count changes.
+              key: ValueKey<int>(_count),
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ),
+          ElevatedButton(
+            child: const Text('Increment'),
+            onPressed: () {
+              setState(() {
+                _count += 1;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
