cab678cb34e911bfe892255692cbcd4d6b93d82a
vitaliiboiko360 <vukelan@gmail.com>
2026-04-29 01:28:51 +0300

    correct signature

--- lib/logo/logo_holder.dart
+++ lib/logo/logo_holder.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class LogoHolder extends StatelessWidget {
   @override
-  Widget build(Object context) {
+  Widget build(BuildContext context) {
     return Image.asset('img/logo.png', width: 120, height: 120);
   }
 }
+
+//, width: 120, height: 120
