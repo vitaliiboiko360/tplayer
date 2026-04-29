99a4abede9d5776c1188a0b7a3afd86aaa65bc50
vitaliiboiko360 <vukelan@gmail.com>
2026-04-30 01:24:39 +0300

    couple logo with text

--- lib/logo/logo_holder.dart
+++ lib/logo/logo_holder.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:tplayer/ui/test_logo.dart';
 
 class LogoHolder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Image.asset('img/logo.png', width: 120, height: 120);
+    return SizedBox(
+      height: 120,
+      width: 360,
+      child: Stack(
+        fit: StackFit.loose,
+        children: [
+          Image.asset('img/logo.png', width: 120, height: 120),
+          Positioned(
+            left: 80,
+            top: 55,
+            child: Align(
+              child: SizedBox(
+                width: 250,
+                height: 60,
+                child: FittedBox(
+                  fit: BoxFit
+                      .scaleDown, // Scales down only if it exceeds the box
+                  child: LongShadowText(text: 'Language Shadowing'),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
